cfc0af9e8eb71fb1da2b8755c944d53ca382df89
fraaguilera <francisco.aguilera.warden@gmail.com>
2019-09-03 10:38:53 -0400

    #CP-11, #CP-12

---
+++ Complete_code/complete_code.py
@@ -0,0 +1,175 @@
+import cv2
+#import matplotlib.pyplot as plt
+import face_recognition 
+import os
+import numpy as np
+from PIL import Image
+
+
+path = './DataBase/'
+new_path = './New_Data/'
+know_faces = []
+know_name = []
+def econdingFacesDB():
+    #Encoding photos and create list  
+    for file in os.listdir(path):
+        name = file.split('.jpg')[0]
+        face = face_recognition.load_image_file(path + file)
+        # face_location = face_recognition.face_locations(face)
+        face_encoding = face_recognition.face_encodings(face)[0]
+        know_name.append(name)
+        know_faces.append(face_encoding)
+        print(know_name)
+    return (know_faces,know_name)
+
+def CompareFaces(newFrame,knowFacesList):
+    
+    finaleResult = " "
+    face_name = ""
+    new_face = face_recognition.load_image_file(newFrame)
+    new_face_encoding = face_recognition.face_encodings(new_face)[0]
+    
+    #Comparing faces
+    resutls = face_recognition.compare_faces(knowFacesList[0],new_face_encoding)
+    #Adding the name of the photo
+    for i in range(len(resutls)):
+        if resutls[i] == True:
+            finaleResult = "True"
+            face_name = knowFacesList[1][i]
+            break
+        else:
+            finaleResult = "False"
+        
+            
+    if finaleResult == "True":
+        return (True,face_name)
+    else:
+        return (False,'No Encontrado')
+
+def RegisterFace(frame):
+    image = face_recognition.load_image_file(frame)
+    face_locations = face_recognition.face_locations(image)
+   
+
+
+    username = input("Ingrese Username: ")
+
+    for face_location in face_locations:
+        top, right, bottom, left = face_location
+
+        face_image = image[top:bottom, left:right]
+        pil_image = Image.fromarray(face_image)
+        pil_image.save('./DataBase/'+username+'.jpg')
+        face = face_recognition.load_image_file('./DataBase/'+username+'.jpg')
+        face_encoding = face_recognition.face_encodings(face)[0]
+        
+
+    know_faces.append(face_encoding)
+    know_name.append(username)
+    
+   
+    
+
+def recognition(namelist,facelist):
+
+
+    video_capture = cv2.VideoCapture(0)
+    # Initialize some variables
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    process_this_frame = True
+
+    while True:
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
+
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        # Only process every other frame of video to save time
+        if process_this_frame:
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(facelist, face_encoding)
+                name = "Unknown"
+
+
+                # If a match was found in known_face_encodings, just use the first one.
+                if True in matches:
+                    first_match_index = matches.index(True)
+                    name = namelist[first_match_index]
+
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(facelist, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = namelist[best_match_index]
+
+                face_names.append(name)
+
+        process_this_frame = not process_this_frame
+
+
+        # Display the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        # Display the resulting image
+        cv2.imshow('Video', frame)
+
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    #Reading and encoding the DB
+    faces = econdingFacesDB()
+
+    #Loading photos to compare
+    obama2_image = face_recognition.load_image_file('./New_Data/obama2.jpeg')
+    #obama2_location = face_recognition.face_locations(obama2_image)
+    #obama2_face_encoding = face_recognition.face_encodings(obama2_image)[0]
+    unknown_image = './New_Data/fra.jpg'
+    #unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+    macri_image = face_recognition.load_image_file('./New_Data/macri.jpg')
+    
+    final_compare_face = CompareFaces(unknown_image,faces)
+
+    #final_compare_face = CompareFaces(web_photo,faces)
+    if final_compare_face[0] == True:
+        print("This is your user name: ",final_compare_face[1])
+        recognition(know_name,know_faces)
+    else:
+        print(final_compare_face[1])
+        RegisterFace(unknown_image)
+        recognition(know_name,know_faces)
+
+
+
+    
+    
